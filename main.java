ff4bae50e7564cf565c5cb52806304e7e3322548
Samuel Gutierrez Lopez <chamsgl6@gmail.com>
2026-05-30 20:17:44 -0600

    Primer commit

---
+++ main.java
@@ -0,0 +1,11 @@
+//Samuel Gutiérrez
+public class Mi_Clase_1
+{
+    public static void main(String args[])
+    {
+        for(int i = 0; i <= 12; i++)
+        {
+            System.out.print("12 * "+ i + " = " + 12 * i + "\n");
+        }
+    }
+}
